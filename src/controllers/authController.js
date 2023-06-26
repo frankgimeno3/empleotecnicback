2c73eb8e4c56874a67cabfc83070ac7d4d5dd3d0
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-26 22:50:29 +0200

    añado contenido al context

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,44 +1,62 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import config from '../config.js';
-import User from '../models/users.model.js';
-import cookie from 'cookie';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import config from "../config.js";
+import User from "../models/users.model.js";
+import cookie from "cookie";
 
 export const register = async (req, res) => {
-  try {const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: 'Ya existe un usuario con el mismo correo electrónico' });
+      return res
+        .status(400)
+        .json({
+          message: "Ya existe un usuario con el mismo correo electrónico",
+        });
     }
-    if (!password) {return res.status(400).json({ message: 'La contraseña es requerida' });
+    if (!password) {
+      return res.status(400).json({ message: "La contraseña es requerida" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
     const accessToken = jwt.sign({ userId: newUser._id }, config.secretKey);
-    res.status(201).json({ accessToken });} catch (error) {console.error(error);
-    res.status(500).json({ message: 'Ha ocurrido un error al registrar el usuario' });
+    res.status(201).json({ accessToken });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Ha ocurrido un error al registrar el usuario" });
   }
 };
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) {return res.status(401).json({ message: 'Usuario inválido' });
+    if (!user) {
+      return res.status(401).json({ message: "Usuario inválido" });
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Contra inválidas' });
+      return res.status(401).json({ message: "Contra inválidas" });
     }
     const accessToken = jwt.sign({ userId: user._id }, config.secretKey);
-    
-    res.status(200)
-    .setHeader('Set-Cookie', cookie.serialize('jwt', accessToken, {
-      httpOnly:true}))
+
+    res
+      .status(200).json({
+        id: user._id,
+        name: user.fullname
+            })
+      .setHeader(
+        "Set-Cookie",
+        cookie.serialize("jwt", accessToken, {
+          httpOnly: true,
+        })
+      )
       .json({ accessToken });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Ha ocurrido un error al iniciar sesión' });
+    res.status(500).json({ message: "Ha ocurrido un error al iniciar sesión" });
   }
 };
-
